ceab309f0ba948256ea4830067619f94be8acfa5
Shirin <shirin.monzavi.1372@gmail.com>
2024-07-25 16:10:20 +0330

    remove id

--- Forecast.Domain/Entities/Forecasts/HourlyDto.cs
+++ Forecast.Domain/Entities/Forecasts/HourlyDto.cs
@@ -3,7 +3,6 @@ namespace Forecast.Domain.Entities.Forecasts;
 
 public record HourlyDto
 {
-    public int Id { get; init; }
     public List<string> Time { get; init; }
     public List<double> Temperature_2m { get; init; }
     public List<int> Relativehumidity_2m { get; init; }
